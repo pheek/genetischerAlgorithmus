02a18d43b926d4eb64fc1c9c2d05e42777587c1b
phi <phi@freimann.eu>
2024-01-30 19:36:14 +0100

    Fitness nach MinMax linear

--- Fitness.py
+++ Fitness.py
@@ -11,13 +11,28 @@ import Point as pt
 import math
 import PointList
 
+A_MIN = 0.00001
+A_MAX = 1.0
+B_MIN = -2000
+B_MAX = 4000
+C_MIN = 0
+C_MAX = 2000
+
 class Fitness:
 
 	def __init__(self):
 		return
 
+	def stretch(v, min, max):
+		a= max-min
+		return a*v+min
+	
 	def saturation(self, a, b, c, x):
-		return c*4000 + 8000*(b-0.5) * math.exp(x * math.log(a))
+		AA = Fitness.stretch(a, A_MIN, A_MAX)
+		BB = Fitness.stretch(b, B_MIN, B_MAX)
+		CC = Fitness.stretch(c, C_MIN, C_MAX)
+		
+		return CC + BB * math.exp(x * math.log(AA))
 
 	def fitnessFunction(self, pl, gen):
 		diffSum = 0
